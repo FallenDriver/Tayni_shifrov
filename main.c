3055dbade4be452f9adfd88adba3c2b4c80a4f0f
Fallen_Driver <agen14syes@gmail.com>
2026-05-17 15:24:18 +0700

    Added base key generation

--- main.c
+++ main.c
@@ -1,8 +1,41 @@
 #include <stdio.h>
+#include <openssl/evp.h>
+#include <openssl/err.h>
+#include <openssl/ec.h>
+
+
 
 
 
 int main() {
-    printf("Hello, world!");
+    
+    if (OPENSSL_init_crypto(0, NULL) != 1) {
+        printf("Ошибка инициализации OpenSSl\n");
+        return 1;
+    }
+
+    printf("Версия OpenSSL: %s\n", OpenSSL_version(OPENSSL_VERSION));
+
+    EVP_PKEY_CTX *ctx;
+
+    ctx = EVP_PKEY_CTX_new_from_name(NULL, "EC", NULL);
+    if (EVP_PKEY_keygen_init(ctx) <= 0) {
+        printf("Ошибка генерации ключа\n");
+        return 1;
+    }
+
+    if (EVP_PKEY_CTX_set_group_name(ctx, "secp256k1") == 0) {
+        printf("Ошибка генерации\n");
+        return 1;
+    }
+
+    EVP_PKEY *pkey = NULL;
+    EVP_PKEY_keygen(ctx, &pkey);
+    
+    
+    EVP_PKEY_CTX_free(ctx);
+    EVP_PKEY_free(pkey);
+
+    OPENSSL_cleanup();
     return 0;
 }
